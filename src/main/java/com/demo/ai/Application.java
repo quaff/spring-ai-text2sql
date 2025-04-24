f6e10f1c6a87ed5685acaf8e900a25de4bb34ec8
Yanming Zhou <zhouyanming@gmail.com>
2025-04-24 11:25:52 +0800

    Polishing

--- src/main/java/com/demo/ai/Application.java
+++ src/main/java/com/demo/ai/Application.java
@@ -1,6 +1,8 @@
 package com.demo.ai;
 
 import com.demo.ai.text2sql.Text2Sql;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,9 +10,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
-		Text2Sql client = new SpringApplicationBuilder(Application.class).web(WebApplicationType.NONE).run(args).getBean(Text2Sql.class);
-		System.out.println(client.query("谁考的分最高？谁考的分最少？平均分又是多少？"));
+		Text2Sql text2Sql = new SpringApplicationBuilder(Application.class).web(WebApplicationType.NONE).run(args).getBean(Text2Sql.class);
+		String question = "谁考的分最高？谁考的分最少？平均分又是多少？";
+		logger.info("用户提问：\n{}", question);
+		String answer = text2Sql.query(question);
+		logger.info("AI回答：\n{}", answer);
 	}
 
 }
