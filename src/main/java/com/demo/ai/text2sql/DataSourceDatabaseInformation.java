b91f669fd0723bf8735a933146f38311a34b72bd
Yanming Zhou <zhouyanming@gmail.com>
2025-04-27 10:50:02 +0800

    Introduce DatabaseInformationProperties to allow user specify configuration

--- src/main/java/com/demo/ai/text2sql/DataSourceDatabaseInformation.java
+++ src/main/java/com/demo/ai/text2sql/DataSourceDatabaseInformation.java
@@ -1,6 +1,9 @@
 package com.demo.ai.text2sql;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.PatternMatchUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -10,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@EnableConfigurationProperties(DatabaseInformationProperties.class)
 public class DataSourceDatabaseInformation implements DatabaseInformation {
 
     private final String databaseProductName;
@@ -18,29 +22,52 @@ public class DataSourceDatabaseInformation implements DatabaseInformation {
 
     private final String tableSchemas;
 
-    public DataSourceDatabaseInformation(DataSource dataSource) throws Exception {
+    public DataSourceDatabaseInformation(DatabaseInformationProperties databaseInformationProperties, DataSource dataSource) throws Exception {
         try (Connection con = dataSource.getConnection()) {
             DatabaseMetaData dmd = con.getMetaData();
-            this.databaseProductName = dmd.getDatabaseProductName();
-            this.databaseVersion = "%d.%d".formatted(dmd.getDatabaseMajorVersion(), dmd.getDatabaseMinorVersion());
-            List<Table> tables = new ArrayList<>();
-            try (ResultSet rs = dmd.getTables(con.getCatalog(), null, null,
-                    new String[]{"TABLE"})) {
-                while (rs.next()) {
-                    String tableName = rs.getString("TABLE_NAME");
-                    List<Column> columns = new ArrayList<>();
-                    try (ResultSet cols = dmd.getColumns(null, null, tableName, null)) {
-                        while (cols.next()) {
-                            columns.add(
-                                    new Column(cols.getString("COLUMN_NAME"), cols.getString("TYPE_NAME"), cols.getString("REMARKS"))
-                            );
-                        }
-                    }
-                    tables.add(new Table(tableName, rs.getString("REMARKS"), rs.getString("TABLE_CAT"), rs.getString("TABLE_SCHEM"), columns));
-                }
+            if (databaseInformationProperties.getDatabaseProductName() != null) {
+                this.databaseProductName = databaseInformationProperties.getDatabaseProductName();
+            } else {
+                this.databaseProductName = dmd.getDatabaseProductName();
             }
-            this.tableSchemas = tables.toString();
+            if (databaseInformationProperties.getDatabaseVersion() != null) {
+                this.databaseVersion = databaseInformationProperties.getDatabaseVersion();
+            } else {
+                this.databaseVersion = "%d.%d".formatted(dmd.getDatabaseMajorVersion(), dmd.getDatabaseMinorVersion());
+            }
+            if (databaseInformationProperties.getTableSchemas() != null) {
+                this.tableSchemas = databaseInformationProperties.getTableSchemas();
+            }
+            else {
+                List<Table> tables = new ArrayList<>();
+                try (ResultSet rs = dmd.getTables(con.getCatalog(), null, null,
+                        new String[]{"TABLE"})) {
+                    while (rs.next()) {
+                        String tableName = rs.getString("TABLE_NAME");
+                        if (!isAllowed(databaseInformationProperties.getTablePatterns(), tableName)) {
+                            continue;
+                        }
+                        List<Column> columns = new ArrayList<>();
+                        try (ResultSet cols = dmd.getColumns(null, null, tableName, null)) {
+                            while (cols.next()) {
+                                columns.add(
+                                        new Column(cols.getString("COLUMN_NAME"), cols.getString("TYPE_NAME"), cols.getString("REMARKS"))
+                                );
+                            }
+                        }
+                        tables.add(new Table(tableName, rs.getString("REMARKS"), rs.getString("TABLE_CAT"), rs.getString("TABLE_SCHEM"), columns));
+                    }
+                }
+                this.tableSchemas = tables.toString();
+            }
         }
+    }
+
+    private static boolean isAllowed(List<String> tablePatterns, String tableName) {
+        if (CollectionUtils.isEmpty(tablePatterns)) {
+            return true;
+        }
+        return PatternMatchUtils.simpleMatch(tablePatterns.toArray(new String[0]), tableName);
     }
 
     @Override
