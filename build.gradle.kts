cfabdecdd27c3baab84b39c1a19f62ced98e9981
Yanming Zhou <zhouyanming@gmail.com>
2025-05-15 11:06:20 +0800

    Add ollama

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation(platform("org.springframework.ai:spring-ai-bom:1.0.0-SNAPSHOT"))
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
 	implementation("org.springframework.ai:spring-ai-starter-model-deepseek")
+	// implementation("org.springframework.ai:spring-ai-starter-model-ollama")
 	implementation("com.github.jsqlparser:jsqlparser:5.1")
 	runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("com.mysql:mysql-connector-j")
