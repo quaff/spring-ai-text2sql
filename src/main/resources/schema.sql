cc990f4662b5c6a29874a867999609879bc34b94
Yanming Zhou <zhouyanming@gmail.com>
2025-05-19 15:54:50 +0800

    Use foreign key reference instead of column comment

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,15 +3,15 @@ DROP TABLE IF EXISTS class;
 DROP TABLE IF EXISTS grade;
 
 CREATE TABLE grade (
-    id INTEGER COMMENT '年级ID',
+    id INTEGER,
     name VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE class (
-    id INTEGER COMMENT '班级ID',
+    id INTEGER,
     name VARCHAR(100) NOT NULL,
-    grade_id INTEGER COMMENT '年级ID',
+    grade_id INTEGER,
     PRIMARY KEY (id),
     CONSTRAINT fk_gradle_id FOREIGN KEY (grade_id) REFERENCES grade(id)
 );
@@ -20,7 +20,7 @@ CREATE TABLE exam_scores (
     id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     score INTEGER NOT NULL,
-    class_id INTEGER COMMENT '班级ID',
+    class_id INTEGER,
     PRIMARY KEY (id),
     CONSTRAINT fk_class_id FOREIGN KEY (class_id) REFERENCES class(id)
 );
